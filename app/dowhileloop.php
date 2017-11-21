7c3f05c8dd55bc31acd7002ea71c8fb37c0dbabe
jthinlay <jthinlay@hotmail.com>
2017-11-21 22:39:14 +0000

    created php do while loop

---
+++ app/dowhileloop.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='UTF-8'>
+        <title>Do While Loop</title>
+    </head>
+    <body>
+        <?php
+            $x = 10;
+            do {
+                echo "Hi there <br/>";
+                $x++;
+            }
+            while($x <= 5);
+        ?>
+    </body>
+</html>
