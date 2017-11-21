265a3d0159218a601412a927fff5d600625bbd8e
jthinlay <jthinlay@hotmail.com>
2017-11-21 22:31:43 +0000

    created php whileloop

---
+++ app/whileloop.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='UTF-8'>
+        <title>while loop</title>
+    </head>
+    <body>
+        <?php
+            $x = 0;
+            while ($x < 5){
+            echo 'while loop start from 0 ended lesser than 5 <br/>';
+            $x++;
+            }
+        ?>
+    </body>
+</html>
