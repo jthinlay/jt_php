84b7543e6f074420a53db2d8c1c5a4de651b5fcc
jthinlay <jthinlay@hotmail.com>
2017-11-21 23:27:39 +0000

    crated php each loop

---
+++ app/eachloop.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Each Loop with Array</title>
+    </head>
+    <body>
+        <?php
+            $array = array('Jim', 'Mike', 'Diana', 'Fray', 'Susan', 'John');
+            foreach($array as $i){
+                echo 'My name is '. $i.'<br/>';
+            }
+        ?>
+    </body>
+</html>
