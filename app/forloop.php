301318a42389f7e584a32fa207cf01cdda02df59
jthinlay <jthinlay@hotmail.com>
2017-11-21 22:45:19 +0000

    created php for loop

---
+++ app/forloop.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='UTF-8'>
+        <title>For Loop</title>
+    </head>
+    <body>
+        <?php
+            for($x = 0; $x <= 10; $x++){
+                echo "Hi there!<br/>";
+            }
+                
+        ?>
+    </body>
+</html>
