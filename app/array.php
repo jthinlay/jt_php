467e33fe32df431ddd3d6998db4180049d3978b8
jthinlay <jthinlay@hotmail.com>
2017-11-21 23:35:14 +0000

    created php array

---
+++ app/array.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='UTF-8'>
+        <title>Array</title>
+    </head>
+    <body>
+        <?php
+            $array = array('Mike', 'Fred', 'Col', 'Nancy', 'Dori');
+            echo $array[0];
+        ?>
+    </body>
+</html>
